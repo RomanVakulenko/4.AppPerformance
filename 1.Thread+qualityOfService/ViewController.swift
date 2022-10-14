ef00b34ccacdce5f4237a26916b0b68bd3fa2671
RomanVakulenko <vakula_48@mail.ru>
2022-10-14 14:24:21 +0300

    3variantsCreateNewThread

--- 1.Thread+qualityOfService/ViewController.swift
+++ 1.Thread+qualityOfService/ViewController.swift
@@ -11,17 +11,38 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // пример того, что асинхронный код зависит от RunLoop и конкурирует с другими задачами за ресурсы в потоке
-                Timer.scheduledTimer(withTimeInterval: 0.5, repeats: true) { _ in
-                    print(Date())
-                }
-                //в этом примере принт sleep(1),который происходил каждую секунду, и принт даты, каждые полсекунды, конфликтовали за ресурсы в главном потоке
-                Timer.scheduledTimer(withTimeInterval: 1, repeats: true) { _ in
-                    print(sleep(1))
-                }
-                //если есть вероятность того, что ресурсов на выполнение всех задач не хватит, то надо вынести какой-то код в другой поток
+        for _ in 0...10 {
+            print("sleep(1)")
+        }
+// 1ый вариант как перевести код в другой поток
+        Thread.detachNewThread {
+            for _ in 0...10 {
+                print("----------")
+            }
+        }
+// 2ой вариант как перевести код в другой поток (создать экземпляр класса Thread), может управлять
+        let thread2 = Thread {
+            for _ in 0...10 {
+                print("+++++++++++++++++")
+            }
+        }
+        thread2.start() //нужно, чтобы запустить поток, если сразу вызвать .cancel, то secondThread не запустится, потому нужен "промежуточный код" (или любой иной код):
+        sleep(1)
+        thread2.cancel()
+
+
+// 3ий вариант как перевести код в другой поток (инкапсулировать сложную логику в отдельный класс, наследованный от класса Thread, override func main)
+        let thread3 = Thread3()
+        thread3.start()
+        sleep(1)
+        thread3.cancel()
     }
-
-
 }
 
+class Thread3: Thread {
+    override func main() {
+        for _ in 0...10 {
+            print("-----Thread3-----")
+        }
+    }
+}
