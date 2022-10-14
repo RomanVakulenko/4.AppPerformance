48c4585c624e6a39e540d6ecfc7a11542d9de106
RomanVakulenko <vakula_48@mail.ru>
2022-10-14 13:23:08 +0300

    newBranch+ignoreDeletedHere

--- 1.Thread+qualityOfService/ViewController.swift
+++ 1.Thread+qualityOfService/ViewController.swift
@@ -11,7 +11,15 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // dev§
+        // пример того, что асинхронный код зависит от RunLoop и конкурирует с другими задачами за ресурсы в потоке
+                Timer.scheduledTimer(withTimeInterval: 0.5, repeats: true) { _ in
+                    print(Date())
+                }
+                //в этом примере принт sleep(1),который происходил каждую секунду, и принт даты, каждые полсекунды, конфликтовали за ресурсы в главном потоке
+                Timer.scheduledTimer(withTimeInterval: 1, repeats: true) { _ in
+                    print(sleep(1))
+                }
+                //если есть вероятность того, что ресурсов на выполнение всех задач не хватит, то надо вынести какой-то код в другой поток
     }
 
 
